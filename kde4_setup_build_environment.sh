1064ebe5b784f083d738ecbcf7e4b36bb22e6e1c
Milian Wolff <mail@milianw.de>
2009-04-23 12:05:01 +0200

    I have to compile my own qt for now...

--- kde4_setup_build_environment.sh
+++ kde4_setup_build_environment.sh
@@ -6,24 +6,24 @@
 ##
 ## If you don't use a separate user, the first section with the
 ## environment variables should go into a separate script.
- 
+
 prepend() { [ -d "$2" ] && eval $1=\"$2\$\{$1:+':'\$$1\}\" && export $1 ; }
- 
+
 # Other
 # you might want to set a full value for PATH rather than prepend'ing, to make
 # sure the your kde3 path isn't in here.
 #prepend PATH /usr/local/bin
- 
+
 # Qt
 # only set Qt related variables if you compiled Qt on your own
 # (which is discouraged). if you use the distro provided Qt, skip
 # this section. Comment it if necessary.
-#export QTDIR=$HOME/kde/src/qt-copy
-#prepend PATH $QTDIR/bin
-#prepend LD_LIBRARY_PATH $QTDIR/lib
-#prepend PKG_CONFIG_PATH $QTDIR/lib/pkgconfig
- 
- 
+export QTDIR=$HOME/projects/compiled/
+prepend PATH $QTDIR/bin
+prepend LD_LIBRARY_PATH $QTDIR/lib
+prepend PKG_CONFIG_PATH $QTDIR/lib/pkgconfig
+
+
 # KDE
 
 # this is where the build files are written to
@@ -43,31 +43,31 @@ prepend PKG_CONFIG_PATH $KDEDIR/lib/pkgconfig
 prepend QT_PLUGIN_PATH $KDEDIR/lib/kde4/plugins
 
 prepend CMAKE_PREFIX_PATH $KDEDIR
- 
+
 # CMake
 # Make sure CMake searches the right places.
 prepend CMAKE_LIBRARY_PATH $KDEDIR/lib
 prepend CMAKE_INCLUDE_PATH $KDEDIR/include
- 
+
 # DBus
 # only set DBUS related variables if you compiled dbus on your own
 # (which is really discouraged). if you use the distro provided dbus,
 # skip this variable. Uncomment it if necessary.
 #export DBUSDIR=$KDEDIR
 #prepend PKG_CONFIG_PATH $DBUSDIR/lib/pkgconfig
- 
+
 # only needed on some strange systems for compiling Qt. do not set
 # it unless you have to.
 #export YACC='byacc -d'
- 
+
 # XDG
 #unset XDG_DATA_DIRS # to avoid seeing kde3 files from /usr
 #unset XDG_CONFIG_DIRS
- 
+
 # make the debug output prettier
 export KDE_COLOR_DEBUG=1
 export QTEST_COLORED=1
- 
+
 # Make
 # Tell many scripts how to switch from source dir to build dir:
 export OBJ_REPLACEMENT="s#$KDE_SRC#$KDE_BUILD#"
