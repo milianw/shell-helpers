10768197d44efaa35fd23947c14dac4ecffa1404
Milian Wolff <mail@milianw.de>
2012-03-13 17:02:18 +0100

    improve visualization, show smoothened lines and use better colors

--- show_memory.sh
+++ show_memory.sh
@@ -8,6 +8,8 @@ set xlabel 'snapshot ~1s';
 set ylabel 'memory consumption in kB';
 set key bottom right;
 plot \
-  '$logfile' using 4 w lines title 'RSS', \
-  '$logfile' using 5 w lines title 'Dirty';
+  '$logfile' using 4 w lines title 'RSS' lt 1, \
+  '$logfile' using 4 smooth bezier w lines title 'RSS (smooth)' lt 7, \
+  '$logfile' using 5 w lines title 'Dirty' lt 2, \
+  '$logfile' using 5 smooth bezier w lines title 'Dirty (smooth)' lt 3;
 ";
