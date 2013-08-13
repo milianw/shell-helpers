d4489e1e9304dcd774f38a392d22fca59f7ad017
Milian Wolff <mail@milianw.de>
2013-08-13 18:46:13 +0200

    Make it possible to run track_memory.sh on busybox

--- track_memory.sh
+++ track_memory.sh
@@ -11,7 +11,43 @@
 pid=$1
 sleep=1;
 
-if [[ "$(ps -p $pid 2>/dev/null | grep $pid)" == "" ]]; then
+if [[ "$(pmap --version 2>&1 | grep BusyBox)" ]]; then
+    summer=$(cat <<'AWK'
+{
+    if ($1 == "total") {
+        pss = $3
+        dirty = $4
+    } else if ($8 == "anon" || $7 == "[heap]") {
+        heap += $3
+    } else if ($7 == "[stack]") {
+        stack += $3
+    }
+}
+END {
+    print pss,dirty,heap,stack
+}
+AWK
+)
+else
+    summer=$(cat <<'AWK'
+{
+    if ($1 == "total") {
+        rss = $4
+        dirty = $5
+    } else if ($7 == "anon") {
+        heap += $3
+    } else if ($7 == "stack") {
+        stack += $3
+    }
+}
+END {
+    print rss,dirty,heap,stack
+}
+AWK
+)
+fi
+
+if [[ "$pid" != "" && ! -d /proc/$pid/ ]]; then
   # maybe a command was given
   if [[ "$(which $1)" != "" ]]; then
     echo "launching: $@"
@@ -24,7 +60,7 @@ else
   fi
 fi
 
-if [[ "$(ps -p $pid | grep $pid)" == "" ]]; then
+if [[ "$pid" == "" || ! -d /proc/$pid/ ]]; then
   echo "cannot find program with pid $pid"
   echo "track_memory.sh PID [SLEEP_TIMEOUT]"
   echo
@@ -34,34 +70,24 @@ fi
 
 logfile=mem.log.$pid
 
-echo "# $(ps -o command= -p $pid)" > $logfile
+echo "# $(cat /proc/$pid/cmdline)" > $logfile
 echo "# $sleep" >> $logfile
 
 cat $logfile
 
-read -r -d '' summer <<'AWK'
-{
-  if ($1 == "total") {
-    rss = $4
-    dirty = $5
-  } else if ($7 == "anon") {
-    heap += $3
-  } else if ($7 == "stack") {
-    stack += $3
-  }
-}
-END {
-  print rss,dirty,heap,stack
-}
-AWK
-
-while [[ "$(ps -p $pid | grep $pid)" != "" ]]; do
+while [ -d /proc/$pid/ ]; do
   echo -n "snapshot $pid: "
   pmap -x $pid | awk "$summer" | tee -a $logfile
   sleep $sleep
 done
 
-echo "done tracking, visualizing"
-$(dirname $0)/show_memory.sh "$logfile"
+visualizer=$(dirname $0)/show_memory.sh
+
+if [ -f "$visualizer" ]; then
+    echo "done tracking, visualizing "$(readlink -f "$logfile")
+    "$visualizer" "$logfile"
+else
+    echo "done, generated "$(readlink -f "$logfile")
+fi
 
 # kate: replace-tabs on;
