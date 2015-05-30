a2748a0cd6a6c55c6104d079f361e175aa3a8a2e
Milian Wolff <mail@milianw.de>
2015-05-30 14:07:38 +0200

    KDE touchpad kcm can handle this.

--- lenovofixes.sh
+++ lenovofixes.sh
@@ -5,6 +5,3 @@ xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
-
-syndaemon -t -k -d
-synclient PalmDetect=1
