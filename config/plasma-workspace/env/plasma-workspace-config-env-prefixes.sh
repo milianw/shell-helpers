bd935be5d1bf0c6193df3e79559d9ce4a7bc4137
Milian Wolff <milian.wolff@kdab.com>
2019-06-19 10:40:17 +0200

    Add plasma-workspace-config-env-prefixes.sh
    This should also be symlinked to ~/.xprofile

---
+++ config/plasma-workspace/env/plasma-workspace-config-env-prefixes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export PATH=/home/milian/projects/compiled/other/bin:/home/milian/projects/compiled/kf5/bin:$PATH
+export LIBEXEC_PATH=/home/milian/projects/compiled/kf5/lib:/usr/lib:/usr/local/lib
+export XDG_CONFIG_DIRS=/home/milian/projects/compiled/kf5/etc/xdg:/etc/xdg
+export XDG_DATA_DIRS=/home/milian/projects/compiled/kf5/share:/usr/share:/usr/local/share
+export QT_PLUGIN_PATH=/home/milian/projects/compiled/kf5/lib/plugins:/home/milian/projects/compiled/kf5/lib64/plugins:/home/milian/projects/compiled/kf5/lib/x86_64-linux-gnu/plugins:/usr/lib/qt/plugins
+export QML_IMPORT_PATH=/home/milian/projects/compiled/kf5/lib/qml:/home/milian/projects/compiled/kf5/lib64/qml:/home/milian/projects/compiled/kf5/lib/x86_64-linux-gnu/qml:/usr/lib/qt/qml
+export QML2_IMPORT_PATH=/home/milian/projects/compiled/kf5/lib/qml:/home/milian/projects/compiled/kf5/lib64/qml:/home/milian/projects/compiled/kf5/lib/x86_64-linux-gnu/qml:/usr/lib/qt/qml
+export LD_LIBRARY_PATH=/home/milian/projects/compiled/kf5/lib64:$LD_LIBRARY_PATH
