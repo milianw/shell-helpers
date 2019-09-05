a847b16a48681a9992430b85556bea3a826a0a6d
Milian Wolff <milian.wolff@kdab.com>
2019-09-05 11:07:26 +0200

    Force colored output, even when compiling with ninja

--- icecream_or_no_icecream.sh
+++ icecream_or_no_icecream.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-compiler=/usr/bin/$(basename $0)
+compiler=$(basename $0)
+compiler_path=/usr/bin/$compiler
+compiler_extra_options=
+
+if [ "$compiler" = "gcc" ] || [ "$compiler" = "g++" ]; then
+    compiler_extra_options="$compiler_extra_options -fdiagnostics-color=always"
+elif [ "$compiler" = "clang" ] || [ "$compiler" = "clang++" ]; then
+    compiler_extra_options="$compiler_extra_options -fcolor-diagnostics"
+fi
 
 if [ -z "$USE_ICECREAM" ]; then
-    /usr/bin/ccache "$compiler" "$@"
+    /usr/bin/ccache "$compiler_path" "$@" $compiler_extra_options
 else
-    /usr/lib64/icecream/bin/icecc "$compiler" "$@"
+    /usr/lib64/icecream/bin/icecc "$compiler_path" "$@" $compiler_extra_options
 fi
