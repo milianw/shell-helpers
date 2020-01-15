deff4b603681e75a86f6988c4dc59c1a02630788
Milian Wolff <milian.wolff@kdab.com>
2020-01-15 13:47:26 +0100

    reduce icecream latency for my office machine

--- icecream_is_available.sh
+++ icecream_is_available.sh
@@ -2,4 +2,11 @@
 
 . /etc/icecream.conf
 
-(echo "listcs"; sleep 1; echo "quit") | timeout 2 telnet $ICECREAM_SCHEDULER_HOST 8766 2> /dev/null | grep -Po "jobs=\d+/\d+" | sed -r 's#jobs=[0-9]+/##' | paste -sd+ | bc
+timeout_1=1
+timeout_2=2
+if [[ "$(hostname)" == "milian-kdab2" ]]; then
+    timeout_1=0.5
+    timeout_2=0.5
+fi
+
+(echo "listcs"; sleep $timeout_1; echo "quit") | timeout $timeout_2 telnet $ICECREAM_SCHEDULER_HOST 8766 2> /dev/null | grep -Po "jobs=\d+/\d+" | sed -r 's#jobs=[0-9]+/##' | paste -sd+ | bc
