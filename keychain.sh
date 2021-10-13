531a83d0da333d6e0c6c758bae74aa676e7518a3
Milian Wolff <mail@milianw.de>
2021-10-13 10:08:28 +0200

    add new ssh key

--- keychain.sh
+++ keychain.sh
@@ -5,7 +5,7 @@ if [[ "$(which ksshaskpass)" != "" ]]; then
 fi
 
 if [[ "$USER" != "root" ]]; then
-  keychain --agents ssh ~/.ssh/kde_id_dsa ~/.ssh/milian ~/.ssh/kde_id_v2
+  keychain --agents ssh ~/.ssh/kde_id_dsa ~/.ssh/kdab ~/.ssh/kde_id_v2
 fi
 
 if [[ -f $HOME/.keychain/$HOSTNAME-sh ]]; then
