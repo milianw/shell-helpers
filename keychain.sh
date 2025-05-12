2d0d239456de2c7e5c3be9e52171eef35e394291
Milian Wolff <mail@milianw.de>
2025-05-12 10:17:34 +0200

    Remove --agents ssh from keychain calls

--- keychain.sh
+++ keychain.sh
@@ -6,9 +6,9 @@ fi
 
 if [[ "$USER" != "root" ]]; then
   if [[ "$(hostname)" == "agathemoarbauer" ]]; then
-    keychain -q -Q --agents ssh ~/.ssh/id_ed25519
+    keychain -q -Q ~/.ssh/id_ed25519
   else
-    keychain -q -Q --agents ssh ~/.ssh/kdab ~/.ssh/kde_id_v2
+    keychain -q -Q ~/.ssh/kdab ~/.ssh/kde_id_v2
   fi
 fi
 
